2b8c6d07322e319259ae0aeeb8f2d6ac6e4bd443
SkyFox1212 <jonasgiese06@gmail.com>
2024-12-01 15:26:07 +0100

    Item groups, recipes

--- src/main/java/me/skyfox/tutorialmod/TutorialMod.java
+++ src/main/java/me/skyfox/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package me.skyfox.tutorialmod;
 
 import me.skyfox.tutorialmod.block.ModBlocks;
+import me.skyfox.tutorialmod.item.ModItemGroups;
 import me.skyfox.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
