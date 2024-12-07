95fd3f241efb5d026a17d213528609b6df6c5657
SkyFox1212 <jonasgiese06@gmail.com>
2024-12-07 14:08:18 +0100

    tags

--- src/main/java/me/skyfox/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/me/skyfox/tutorialmod/block/custom/MagicBlock.java
@@ -1,6 +1,7 @@
 package me.skyfox.tutorialmod.block.custom;
 
 import me.skyfox.tutorialmod.item.ModItems;
+import me.skyfox.tutorialmod.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
@@ -34,12 +35,16 @@ public class MagicBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if(entity instanceof ItemEntity itemEntity) {
-            if(itemEntity.getStack().getItem() == ModItems.RAW_PINK_GARNET) {
+            if(isValidItem(itemEntity.getStack())) {
                 itemEntity.setStack(new ItemStack(Items.DIAMOND, itemEntity.getStack().getCount()));
             }
         }
 
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack stack) {
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
